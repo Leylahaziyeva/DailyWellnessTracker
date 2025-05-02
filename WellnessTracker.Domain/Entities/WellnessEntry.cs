feec3d307e2cb1e2a344af96029fb48fcb37d75b
Leyla Haziyeva <leylaih@code.edu.az>
2025-05-02 20:34:44 +0400

    Some changes

--- WellnessTracker.Domain/Entities/WellnessEntry.cs
+++ WellnessTracker.Domain/Entities/WellnessEntry.cs
@@ -1,8 +1,19 @@
-﻿namespace WellnessTracker.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WellnessTracker.Domain.Entities;
 public class WellnessEntry : Entity
 {
     public DateTime? Date { get; set; }
     public int UserId { get; set; }
     public User? User { get; set; }
     public required WellnessDetails Details { get; set; } // JSON mapped
+}
+
+[Owned]  // JSON mapped
+public class WellnessDetails
+{
+    public required string Mood { get; set; }
+    public double SleepHours { get; set; }
+    public required List<string> Activities { get; set; }
+    public required string Notes { get; set; }
 }
